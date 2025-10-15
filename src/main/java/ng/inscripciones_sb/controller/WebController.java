530a939191e5e63461aaeb3b4b31fd557de6f7d7
NicolaIsGaray <nicogaray2713@gmail.com>
2025-10-15 00:55:28 -0300

    Creado WebController Backend
    Controlador para redirigir peticiones

---
+++ src/main/java/ng/inscripciones_sb/controller/WebController.java
@@ -0,0 +1,12 @@
+package ng.inscripciones_sb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
