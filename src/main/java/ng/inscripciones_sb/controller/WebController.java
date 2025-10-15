9a701469d96d13e781bd5bbdd1629d5dcb44595c
NicolaIsGaray <nicogaray2713@gmail.com>
2025-10-15 00:59:06 -0300

    Actualizado WebController

--- src/main/java/ng/inscripciones_sb/controller/WebController.java
+++ src/main/java/ng/inscripciones_sb/controller/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
-    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    @RequestMapping(value = "/{path:^(?!api).*$}")
     public String redirect() {
         return "forward:/index.html";
     }
